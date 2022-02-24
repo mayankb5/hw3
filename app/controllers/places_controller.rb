804a3f31c4a1188eadd53c9400408662b7694f69
mayankb5 <mayank.bhatia@kellogg.northwestern.edu>
2022-02-24 03:35:51 +0000

    Controller Setup

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,23 @@
 class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+    end
+
+    def show
+        @place = Place.find(params["id"])
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Pace.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
+
+
 end
+
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params["id"])
+    end
+
+    def new
+        @post = Post.new
+        @post.place_id = params["place_id"]
+    end
+
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/posts"
+    end
+
+
+
 end
